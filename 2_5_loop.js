781769496673af95cc542f8ea1328d19b911af24
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2025-10-03 17:52:09 +0530

    find largest no in array done

--- 2_5_loop.js
+++ 2_5_loop.js
@@ -1,4 +1,4 @@
-// write a function that searches for an element in an array and returns the index, if the element is not present then return -1
+// 1write a function that searches for an element in an array and returns the index, if the element is not present then return -1
 
 let arr = [3, 5, 6, 8, 9, 0]
 
@@ -21,7 +21,7 @@ console.log(searchElement1(arr, 9))
 
 
 
-// write a functions that return the numbers of negative numbers in array
+//2 write a functions that return the numbers of negative numbers in array
 
 function negativeCount(arr) {
     let count = 0
@@ -35,3 +35,18 @@ function negativeCount(arr) {
 
 let arr2 = [1, -4, -6, 5, -7]
 console.log(negativeCount(arr2))
+
+
+// 3). write a function that return largest number in the array
+
+function findLargest(arr) {
+    let max = -Infinity;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    return max
+}
+
+console.log(findLargest(arr2))
--- question.js
+++ question.js
@@ -1,4 +1,6 @@
-// write a function that searches for an element in an array and returns the index, if the element is not present then return -1
+// 1) write a function that searches for an element in an array and returns the index, if the element is not present then return -1
 
 
-// write a functions that return the numbers of negative numbers in array
+// 2) write a functions that return the numbers of negative numbers in array
+
+// 3). write a function that return largest number in the array
